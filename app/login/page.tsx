d3c3a3fb0375cd31c4e3619aee80ab5d91fe86c8
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-05 20:58:01 +0330

    Update page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginTabs from './LoginTabs';
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center px-4">
+      <LoginTabs />
+    </div>
+  );
+}
